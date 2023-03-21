0d1674da6741e4a0da4c2d8cfa774b87cf7edb4d
Kode Navigator <chumex412@gmail.com>
2023-03-21 03:48:53 +0100

    adds entity for destination

---
+++ src/application/domain/entity/destination.ts
@@ -0,0 +1,8 @@
+export type DestinationNamesTypes = {
+	name: string;
+	id: number;
+};
+
+export type TabsPropTypes = {
+	tabs: DestinationNamesTypes[];
+};
---
+++ src/application/domain/entity/travel-log.ts
@@ -0,0 +1,43 @@
+export interface TravelLog {
+	name: string;
+	images: TravelLogImagesType;
+}
+
+export interface Destination extends TravelLog {
+	description: string;
+	distance: string;
+	travel: string;
+}
+
+type TravelLogImagesType = {
+	png?: string;
+	webp?: string;
+	portrait?: string;
+	landscape?: string;
+};
+
+export interface Crew extends TravelLog {
+	role: string;
+	bio: string;
+}
+
+export interface TechItem extends TravelLog {
+	description: string;
+}
+
+export type TravelLogDataType = {
+	destinations: Destination[];
+	crew: Crew[];
+	technology: TechItem[];
+};
+
+export type LogArrayType = Destination[] | Crew[] | TechItem[];
+
+export type LogObjType = Destination | Crew | TechItem;
+
+export type LogPropType = 'destinations' | 'crew' | 'technology';
+
+export type PageCaptionPropType = {
+	text: string;
+	count: number;
+};
