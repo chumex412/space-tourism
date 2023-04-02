0de850ade692892b556b8e87dc48c2d7b5ff7e7a
Kode Navigator <chumex412@gmail.com>
2023-04-02 21:45:25 +0100

    adjusts class values for responsiveness

--- src/components/ui/header/HeaderGroup.tsx
+++ src/components/ui/header/HeaderGroup.tsx
@@ -10,7 +10,7 @@ const HeaderGroup = ({ intro, lead, text }: PageHeader) => {
 				{intro && (
 					<span
 						className={`mb-6 font-barlow-cond font-normal ${
-							pathname === 'crew' ? 'text-lg' : 'text-md'
+							pathname === '/crew' ? 'text-lg' : 'text-md'
 						} leading-[120%] text-secondary`}
 					>
 						{intro}
@@ -18,9 +18,9 @@ const HeaderGroup = ({ intro, lead, text }: PageHeader) => {
 				)}
 				<strong
 					className={`mb-6 font-bellefair font-normal uppercase ${
-						pathname === 'destination'
+						pathname === '/destination'
 							? 'text-2xl'
-							: pathname === 'crew' || pathname === 'technology'
+							: pathname === '/crew' || pathname === '/technology'
 							? 'text-xl'
 							: 'text-3xl'
 					} leading-[115%] text-white`}
@@ -28,7 +28,13 @@ const HeaderGroup = ({ intro, lead, text }: PageHeader) => {
 					{lead}
 				</strong>
 			</h2>
-			<p className="w-[444px] font-barlow text-base font-normal leading-[200%] text-secondary">{text}</p>
+			<p
+				className={`w-full max-w-[444px] font-barlow text-base font-normal leading-[200%] text-secondary ${
+					pathname === '/destination' || pathname === '/crew' ? 'mx-auto xl:mx-0' : ''
+				}`}
+			>
+				{text}
+			</p>
 		</hgroup>
 	);
 };
--- src/components/ui/page-caption/PageCaption.tsx
+++ src/components/ui/page-caption/PageCaption.tsx
@@ -2,7 +2,7 @@ import { PageCaptionPropType } from '../../../application/domain/entity/travel-l
 
 const PageCaption = ({ text, count }: PageCaptionPropType) => {
 	return (
-		<header>
+		<header className="mb-14 py-3 text-center md:text-left">
 			<h1 className="font-barlow-cond text-md font-normal uppercase leading-[120%] tracking-[4.725px] text-white">
 				<span className="font-bold opacity-25 mix-blend-normal">0{count}</span> <span>{text}</span>
 			</h1>
