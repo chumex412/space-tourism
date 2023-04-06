7403eab04e0b2f37aa71d3f5399b34b5db805dd1
Kode Navigator <chumex412@gmail.com>
2023-04-06 22:21:20 +0100

    updates to first index when landing on this page

--- src/application/controller/crewController.ts
+++ src/application/controller/crewController.ts
@@ -1,22 +1,29 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useEffect, useCallback, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Crew } from '../domain/entity/travel-log';
 import { AppState } from '../redux/store';
 import { getCrew, getCrewCurrentIndex } from '../redux/travel-log/selector';
 import { addCrew, updateCrewIndex } from '../redux/travel-log/slice';
 import { getTravelLog } from '../services/travel-log';
-import { changeCrew, getCrewCount } from '../usecase/travel-log';
+import { getTravelCount } from '../usecase/travel-log';
 
 const useTravelCrew = function () {
 	const dispatch = useDispatch();
 	const crew = useSelector((state: AppState) => getCrew(state)) as Crew[];
 	const currentIndex = useSelector((state: AppState) => getCrewCurrentIndex(state));
 
+	const { pathname } = useLocation();
+
 	const crewCountRef = useRef<string[]>([]);
 
 	useEffect(() => {
 		dispatch(addCrew(getTravelLog('crew')));
 	}, [dispatch]);
+
+	useEffect(() => {
+		if (pathname !== 'crew') dispatch(updateCrewIndex({ index: 0 }));
+	}, [dispatch, pathname]);
 
 	const getSingleCrew = useCallback(
 		(index: number) => {
@@ -25,9 +32,9 @@ const useTravelCrew = function () {
 		[dispatch]
 	);
 
-	crewCountRef.current = getCrewCount(crew);
+	crewCountRef.current = getTravelCount(crew, 'crew');
 
-	return { getSingleCrew, crewCountRef, currentIndex, crew };
+	return { getSingleCrew, crewCountRef, currentIndex, crew, pathname };
 };
 
 export default useTravelCrew;
