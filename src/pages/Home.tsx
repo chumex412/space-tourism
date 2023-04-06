f6c482bfafb2367d9cec93ee2d4c4865c689fcc5
Kode Navigator <chumex412@gmail.com>
2023-04-06 22:36:14 +0100

    make the home page responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
 import { imgs } from '../assets';
 import { PageBackground, HeaderGroup } from '../components/ui';
 
 const Home = () => {
 	return (
-		<>
+		<main className="container flex items-center lg:block xl:h-[85vh] xl:items-end">
 			<PageBackground desktop={imgs.home.desktop} mobile={imgs.home.mobile} tablet={imgs.home.tablet} />
-			<section className="my-auto flex h-5/6 w-full flex-col items-center justify-between text-center lg:flex-row lg:items-end lg:text-left">
+			<section className="flex h-5/6 w-full flex-col items-center justify-between pt-16 pb-6 text-center xl:my-auto xl:flex-row xl:items-end xl:text-left">
 				<HeaderGroup
 					intro="SO, YOU WANT TO TRAVEL TO"
 					lead="Space"
 					text="Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"
 				/>
-				<div className="flex h-[242px] w-[242px] items-center justify-center rounded-full bg-white lg:h-[274px] lg:w-[274px]">
-					<h2 className="font-bellefair text-lg leading-[110%]">Explore</h2>
+				<div className="mt-20 flex h-[150px] w-[150px] items-center justify-center rounded-full bg-white md:mt-32 md:h-[242px] md:w-[242px] xl:mt-0 xl:h-[274px] xl:w-[274px]">
+					<h2 className="font-bellefair text-[1.25rem] leading-[110%] md:text-[2rem]">Explore</h2>
 				</div>
 			</section>
-		</>
+		</main>
 	);
 };
 
