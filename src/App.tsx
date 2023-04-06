a1710976f4dae8c6e4269f819072dab4fc1063b2
Kode Navigator <chumex412@gmail.com>
2023-04-06 22:43:01 +0100

    router inlet container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+import rootRouter from './pages';
 
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className="App">
+			{
+				<Suspense fallback={<div>Loading...</div>}>
+					<RouterProvider router={rootRouter()} />
+				</Suspense>
+			}
+		</div>
+	);
 }
 
 export default App;
